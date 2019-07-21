d67f97d757b664084ebe536082792b3655e392bf
panjisula2 <panji.sula@gmail.com>
2019-07-22 02:46:45 +0700

    add file CobaEnum.java

---
+++ CobaEnum.java
@@ -0,0 +1,10 @@
+enum Color{
+    MERAH, KUNING, HIJAU;
+}
+
+public class CobaEnum{
+    public static void main(String [] args){
+        Color c1 = Color.MERAH;
+        System.out.println(c1);
+    }
+}
