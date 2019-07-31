ff891e2f7594af7622c6570b0c40bfbe5a8fda89
panjisula2 <panji.sula@gmail.com>
2019-07-31 14:43:46 +0700

    add file java CobaAngka dan CobaArray

---
+++ CobaAngka.java
@@ -0,0 +1,8 @@
+public class CobaAngka{
+    public static void main(String[] args){
+        int i = 1000;
+        byte b = (byte) i;
+
+        System.out.println("nilai variabel b sekarang adalah : "+b);
+    }
+}
---
+++ CobaArray.java
@@ -0,0 +1,12 @@
+public class CobaArray{
+    public static void main(String[] args){
+        String[] tes = new String[3];
+        tes[0]="nama";
+        tes[1]="saya";
+        tes[2]="panji";
+
+        for (String coba : tes) {
+            System.out.println(coba);
+        }
+    }
+}
